a55c9eabf9f3650da3c320562fd253a10a37ac37
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2021-02-28 21:17:51 -0300

    feat: add all files moveit

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,119 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import challenges from '../../challenges.json';
+import { LevelUpModal } from '../components/LevelUpModal';
+
+type Challenge = {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
+
+type ChallengesContextData = {
+  level: number;
+  currentExperience: number; 
+  experienceToNextLevel: number;
+  challengesCompleted: number;
+  activeChallenge: Challenge;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+  resetChallenge: () => void;
+  completeChallenge: () => void;
+  closeLevelUpModal: () => void;
+}
+
+type ChallegesProviderProps = {
+  children: ReactNode;
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider({ children, ...rest}: ChallegesProviderProps) {
+  const [level, setLevel] = useState(rest.level ?? 1);
+  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
+
+  const [activeChallenge, setActiveChallenge] = useState(null);
+  const [isLevelUpModalOpen, setIsLevelModalOpen] = useState(false);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
+  function levelUp() {
+    setLevel(level + 1);
+    setIsLevelModalOpen(true);
+  }
+
+  function closeLevelUpModal() {
+    setIsLevelModalOpen(false);
+  }
+
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+    
+    setActiveChallenge(challenge);
+    new Audio('/notification.mp3').play();
+
+    //https://developer.mozilla.org/en-US/docs/Web/API/Notification
+    if (Notification.permission === 'granted') {
+      new Notification('Novo desafio 🎉', {
+        body: `Valendo ${challenge.amount}xp!`
+      });
+    }
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge;
+    let finalExperience  = currentExperience + amount;
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+  }
+
+  useEffect(() => {
+    Notification.requestPermission();
+  }, [])
+
+  useEffect(() => {
+    Cookies.set('level', String(level));
+    Cookies.set('currentExperience', String(currentExperience));
+    Cookies.set('challengesCompleted', String(challengesCompleted));
+  }, [level, currentExperience, challengesCompleted])
+
+  return (
+    <ChallengesContext.Provider 
+      value={{ 
+        level, 
+        currentExperience, 
+        experienceToNextLevel,
+        challengesCompleted, 
+        activeChallenge,
+        levelUp ,
+        startNewChallenge,
+        resetChallenge,
+        completeChallenge,
+        closeLevelUpModal
+      }}
+    >
+      {children}
+      {isLevelUpModalOpen && <LevelUpModal />}
+    </ChallengesContext.Provider>
+  );
+}
